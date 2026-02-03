8216216d256f6a87363c02732ec23227549d457f
abdulmalik299 <58619877+abdulmalik299@users.noreply.github.com>
2026-02-03 18:29:36 +0300

    Delete supabase directory

--- supabase/functions/_shared/cors.ts
+++
@@ -1,6 +0,0 @@
-export const corsHeaders: Record<string, string> = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-headers':
-    'authorization, x-client-info, apikey, content-type, x-requested-with',
-  'access-control-allow-methods': 'GET, POST, OPTIONS',
-};
