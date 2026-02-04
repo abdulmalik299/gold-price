a05b07fda3d60c09269f1855bb1f921b4dcb654a
abdulmalik299 <58619877+abdulmalik299@users.noreply.github.com>
2026-02-04 18:37:57 +0300

    Add FORMSPREE_ENDPOINT to env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,5 @@ export const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZHBvcHBoZW5tYnRpdmR0bGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwOTg1NzAsImV4cCI6MjA4NTY3NDU3MH0.mZCaMiqmZSBDCNXTNQCkfl0-uHv3ozHRvVmhEvi3WeQ'
 
 export const GOLD_API_URL = 'https://api.gold-api.com/price/XAU'
+export const FORMSPREE_ENDPOINT =
+  import.meta.env.VITE_FORMSPREE_ENDPOINT ?? 'https://formspree.io/f/mjgondvv'
